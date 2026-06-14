5054c63fae26ef77edff16c8322b0efff090ddf7
simonstimson-droid <simon.stimson@gmail.com>
2026-06-14 10:11:09 +0100

    Add eclementson@hotmail.com as approved sender

--- scripts/email_auto_processor.py
+++ scripts/email_auto_processor.py
@@ -24,6 +24,7 @@ PROCESSED_LABEL = "FamilyHQ_Processed"
 ALLOWED_SENDERS = [
     "simon.stimson@gmail.com",
     "clementsonemma@gmail.com",
+    "eclementson@hotmail.com",
 ]
 
 # ---- Load token ----
